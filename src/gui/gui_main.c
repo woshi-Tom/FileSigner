50fb70b0494040b9e290e1b681c25939cdafad4a
TOM <2155801257@qq.com>
2026-05-20 02:01:03 +0800

    精简：TSA 测速日志改为 ping 风格——来自 xxx: 延迟 = xx ms

--- src/gui/gui_main.c
+++ src/gui/gui_main.c
@@ -805,21 +805,19 @@ WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             for (int i = 0; i < TSA_SERVER_COUNT; i++) {
                 wchar_t wlabel[256];
                 MultiByteToWideChar(CP_UTF8, 0, g_tsa_servers[i].label, -1, wlabel, 256);
-                log_message(LOG_COLOR_INFO, L"\u6D4B\u8BD5 %s\u4E2D\u2026\u2026", wlabel);
-                UpdateWindow(g_hLog);
 
                 DWORD t0 = GetTickCount();
                 int ok = timestamp_test_server(g_tsa_servers[i].url);
                 DWORD elapsed = GetTickCount() - t0;
 
                 if (ok) {
-                    log_message(LOG_COLOR_OK, L"%s ----- \u5EF6\u8FDF %lu ms", wlabel, elapsed);
+                    log_message(LOG_COLOR_OK, L"\u6765\u81EA %s: \u5EF6\u8FDF = %lu ms", wlabel, elapsed);
                     if (best_idx < 0 || (int)elapsed < best_latency) {
                         best_idx = i;
                         best_latency = (int)elapsed;
                     }
                 } else {
-                    log_message(LOG_COLOR_FAIL, L"%s ----- \u5931\u8D25", wlabel);
+                    log_message(LOG_COLOR_FAIL, L"\u6765\u81EA %s: \u8BF7\u6C42\u8D85\u65F6", wlabel);
                 }
                 UpdateWindow(g_hLog);
             }
